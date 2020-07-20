da8fa7f691930843bd14925451df4247f5bdc4c5
OneCentral <danielmethnerek@gmail.com>
2020-07-20 14:29:15 +0800

    ngif fix

--- ba-monitor-angular/src/app/app.module.ts
+++ ba-monitor-angular/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -31,7 +33,9 @@ import { RegistrationComponent } from './public/registration/registration.compon
     RegistrationComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    FormsModule,
     NgbModule,
     AppRoutingModule,
     FontAwesomeModule
