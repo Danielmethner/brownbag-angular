5c2501c956c45bb456a54ed1cd9e223412bebae0
OneCentral <danielmethnerek@gmail.com>
2020-07-21 21:24:03 +0800

    icons for personal area

--- ba-monitor-angular/src/app/protected/entities/party/person-base/person-base.component.ts
+++ ba-monitor-angular/src/app/protected/entities/party/person-base/person-base.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faChartPie, faCreditCard, faExchangeAlt, faFile, faColumns, faList } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-person-base',
   templateUrl: './person-base.component.html',
   styleUrls: ['./person-base.component.css']
 })
 export class PersonBaseComponent implements OnInit {
-
+  faChartPie = faChartPie;
+  faExchangeAlt = faExchangeAlt;
+  faCreditCard = faCreditCard;
+  faFile = faFile; faColumns = faColumns; faList = faList;
   personName = 'Bernd';
   constructor() { }
 
