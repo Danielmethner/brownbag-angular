da861debdaf44f6dee5fa891bd93f04e6c411bcf
OneCentral <danielmethnerek@gmail.com>
2020-08-05 21:31:55 +0800

    performance optimisation

--- ba-monitor-angular/server.js
+++ ba-monitor-angular/server.js
@@ -1,11 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ba-monitor-angular'));
+app.use(compression()) 
 
 app.get('/*', function(req,res) {
     
--- ba-monitor-angular/src/environments/environment.prod.ts
+++ ba-monitor-angular/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiBaseUrl: 'https://brownbag-api.herokuapp.com',
   production: true
 };
